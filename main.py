c05192bad6c54c4cb573fac91d4098241eb741e8
trunghc-0841 <huynh.chi.trung@sun-asterisk.com>
2019-10-13 11:47:18 +0700

    Main Window

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ import sys
 import os
 import mysql.connector
 from PyQt5.QtWidgets import *
+from PyQt5.QtCore import Qt
+
+current_user = (1, "admin")
 
 
 class Form(QDialog):
@@ -9,6 +12,7 @@ class Form(QDialog):
         super(Form, self).__init__(parent)
         self.alert = Alert(self)
 
+        self.setWindowTitle("LOGIN")
         self.username = QLineEdit(self)
         self.QUserLabel = QLabel("EMAIL")
 
@@ -40,7 +44,11 @@ class Form(QDialog):
         con.commit()
 
         if record:
-            print(record)
+            global current_user
+            current_user = record
+            self.mainWindow = MainWindow()
+            self.mainWindow.show()
+            form.close()
         else:
             self.alert.show()
 
@@ -62,6 +70,36 @@ class Alert(QDialog):
 
     def submit_btn(self):
         Alert.close(self)
+
+
+class MainWindow(QMainWindow):
+    def __init__(self, *args, **kwargs):
+        super(MainWindow, self).__init__(*args, **kwargs)
+
+        self.table = QTableWidget()
+        self.setWindowTitle("WELCOME " + current_user[1])
+
+        self.resize(800, 600)
+
+        # table.setAlignment(Qt.AlignCenter)
+        self.table.setRowCount(3)
+        self.table.setColumnCount(2)
+        self.table.setHorizontalHeaderLabels(["Class name", "Subject name"])
+        self.getAllClass()
+        self.setCentralWidget(self.table)
+
+    def getAllClass(self):
+        con = mysql.connector.connect(user="root", password="123456", host="localhost", database="face_attandance")
+        cursor = con.cursor()
+
+        sql = "Select classes.name, subjects.name from classes inner join subjects on subjects.id = classes.subjectID " \
+              "where teacherID = %s"
+        cursor.execute(sql, (current_user[0],))
+        records = cursor.fetchall()
+
+        for index, row in enumerate(records):
+            self.table.setItem(index, 0, QTableWidgetItem(row[0]))
+            self.table.setItem(index, 1, QTableWidgetItem(row[1]))
 
 
 app = QApplication(sys.argv)
